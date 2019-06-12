3a816d9299ea4e26bb4e20251a45e2ef93b0e4a4
Danny Smith <hi@danny.is>
2019-06-12 18:30:17 +0100

    Initial commit

---
+++ tatteo-customposttypes.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Tatteo Custom Post Types
+ * Plugin URI:      tatteo.com
+ * Description:     A plugin to generate Custom Post Types for Tatteo
+ * Author:          RED Academy
+ * Author URI:      redacademy.com
+ * Text Domain:     tatteo-customposttypes
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Tatteo_Customposttypes
+ */
+
+// Your code starts here.
